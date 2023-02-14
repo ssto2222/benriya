e8abfe640bfdc948d349f5aea0fc7a433ff49b83
tuttofare <tech2metail@gmail.com>
2023-02-14 17:33:08 +0900

    deploy heroku

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if DEBUG:
+if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',

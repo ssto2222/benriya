18acaaeab7163de98f793094dd9c85f70cc8a953
tuttofare <tech2metail@gmail.com>
2023-02-14 19:58:48 +0900

    checkout

--- mainapp/views.py
+++ mainapp/views.py
@@ -227,4 +227,12 @@ def checkout_confirm(request):
     context={"email":user.email}
     return render(request,'mainapp/checkout.html',context)
 
+from django.views.decorators.csrf import requires_csrf_token
+from django.http import HttpResponseServerError
 
+@requires_csrf_token
+def my_customized_server_error(request, template_name='500.html'):
+    import sys
+    from django.views import debug
+    error_html = debug.technical_500_response(request, *sys.exc_info()).content
+    return HttpResponseServerError(error_html)

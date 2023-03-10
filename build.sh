83a86385d703b45e7b2757a3419cb2eec4720438
tuttofare <tech2metail@gmail.com>
2023-03-11 04:33:01 +0900

    build change

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+#python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
